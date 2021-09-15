bdb2b934a14d45d18a6f092af2c34594d1721ecb
Dan Marius Dumitrescu <dumy.tm@gmail.com>
2021-09-15 22:38:12 +0200

    All tests done. ✅

--- consecutive.js
+++ consecutive.js
@@ -1,26 +1,17 @@
-
 function consecutive(chain){
-    let max=0;
-    let current=0;
-    if(chain!=null){
-        for (let value of chain) {
-            if(value==1 || parseInt(value)==1){
-                current++;
-                console.log(current+"oko");
-            }
-            else if(value==0 || parseInt(value)==0){
-                if(current>max){
-                    max=current;
-                    current=0;
-                }
-            }
-            else
-            return -1
-        }
-        return max;
+    let max = 0;
+    let count = 0;
+    
+    for (let element of chain) {
+        if (parseInt(element) === 1) {
+            count++;
+        } else if (parseInt(element) === 0) {
+            max = Math.max(max, count);
+            count = 0;
+        } else return -1;
     }
-    else return -1;
+    max = Math.max(max, count);
+    return max;
 }
-
 
 export {consecutive};
--- test/consecutive.test.js
+++ test/consecutive.test.js
@@ -1,17 +1,21 @@
 import { consecutive } from "../consecutive";
 
 
-describe('DNA Check', () => {
+describe('1s and 0s', () => {
     
-    test(' Given an array of 1 and 0s, find the maximum number of consecutive 1 in this array', () => {
-
-        /*let prove=[0,"1",1,1,1,1,0,0,0,0,"0","1"];
-        expect(consecutive(prove)).toBe(5);*/
+    test('Given an array of 1 and 0s, find the maximum number of consecutive 1 in this array', () => {
         let prove=[0,1,1,"0",1,1,0,0,0,0,"0","1","1","1"];
         expect(consecutive(prove)).toBe(3);
-        prove=[0,1,3,4,1,32,0,0,0,0,"0","1"];
-        expect(consecutive(prove)).toBe(-1);
-        prove=[];
-        expect(consecutive(prove)).toBe(-1);
      });
+     
+    test('If there is any value other than 1 or 0 in the array, this is an error, return -1.', () => {
+        let prove=[0,1,3,4,1,32,0,0,0,0,"0","1"];
+        expect(consecutive(prove)).toBe(-1);
+    });
+
+    test('The 1\'s and 0\'s may be either string or integer values, both are acceptable and should be considered equivalent.', () => {
+        let prove=["0","1"];
+        expect(consecutive(prove[0])).toBe(0);
+    });
+     
 });
